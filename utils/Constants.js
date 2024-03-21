a8c9c6cda3a42d472ea692ecd1d1307e4a261164
Onkar Karale <onkarkarale4@gmail.com>
2024-03-22 01:27:10 +0530

    Fixed the issue of corsproxy

--- utils/Constants.js
+++ utils/Constants.js
@@ -5,10 +5,10 @@ export const SWIGGY_CDN_LINK = 'https://res.cloudinary.com/swiggy/image/upload/f
 export const SWIGGY_API_URL =
 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
 
-export const SWIGGY_API_URL_CORS = 'https://thingproxy.freeboard.io/fetch/https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D18.5204303%26lng%3D73.8567437%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING'
+export const SWIGGY_API_URL_CORS = 'https://thingproxy.freeboard.io/fetch/' + encodeURIComponent('https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING')
 
 export const SWIGGY_MENU_API = 'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5204303&lng=73.8567437&restaurantId='
 
-export const SWIGGY_MENU_API_CORS = 'https://thingproxy.freeboard.io/fetch/https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D18.5204303%26lng%3D73.8567437%26restaurantId%3D'
+export const SWIGGY_MENU_API_CORS = 'https://thingproxy.freeboard.io/fetch/' + encodeURIComponent('https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5204303&lng=73.8567437&restaurantId=')
 
 export const SWIGGY_MENU_IMG_API = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/'
