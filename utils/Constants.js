bea4d8adc43ea24c4ecf1b55607682475bcc1bd7
Onkar Karale <onkarkarale4@gmail.com>
2024-04-01 00:42:13 +0530

    Fixed the cors issue

--- utils/Constants.js
+++ utils/Constants.js
@@ -3,11 +3,11 @@ export const SWIGGY_CDN_LINK = 'https://res.cloudinary.com/swiggy/image/upload/f
 
 // Swiggy API to get Restaurant data with corsproxy
 export const SWIGGY_API_URL =
-'https://instafood.onrender.com/api/restaurants?lat=18.5204303&lng=73.8567437&page_type=DESKTOP_WEB_LISTING'
+'https://food-ordering-app-server.vercel.app/api/proxy/swiggy/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
 
 // export const SWIGGY_API_URL_CORS = 'https://thingproxy.freeboard.io/fetch/' + encodeURIComponent('https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING')
 
-export const SWIGGY_MENU_API = 'https://instafood.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=18.5204303&lng=73.8567437&restaurantId='
+export const SWIGGY_MENU_API = 'https://food-ordering-app-server.vercel.app/api/proxy/swiggy/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5204303&lng=73.8567437&restaurantId='
 
 // export const SWIGGY_MENU_API_CORS = 'https://thingproxy.freeboard.io/fetch/' + encodeURIComponent('https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5204303&lng=73.8567437&restaurantId=')
 
